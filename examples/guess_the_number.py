bf00f1c779b05e3475c7f4651fa5171984564a03
Nobody <156914335+Nobodyasidentity@users.noreply.github.com>
2025-11-05 20:13:19 +0100

    Update guess_the_number.py

--- examples/guess_the_number.py
+++ examples/guess_the_number.py
@@ -8,7 +8,7 @@ def _intinput(s):
         try:return int(a)
         except:print(f"'{a}' is not an integer")
 from random import randint
-def Game(min=1,max=10,a=None):
+def Game(min,max,a=None):
     r=randint(min,max)# a random number from 1 to 10
     while a!=r:
         a=_intinput(f'Guess a number {min} to {max}: 'if a==None else 'Try again: ')
@@ -22,5 +22,6 @@ def _():
     print(api.c.clear+'Guess the number mod!')
 @api.register('main')
 def _():
-    Game()
+    Game(1,10)
     api.exitcode(1)
+    api.fire('exit')
