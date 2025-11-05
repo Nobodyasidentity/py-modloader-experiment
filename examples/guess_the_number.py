729c2b6aad2384e3a1e652597aff60dd5f91d08f
Nobody <156914335+Nobodyasidentity@users.noreply.github.com>
2025-11-05 19:37:35 +0100

    Update guess_the_number.py

--- examples/guess_the_number.py
+++ examples/guess_the_number.py
@@ -8,16 +8,17 @@ def _intinput(s):
         try:return int(a)
         except:print(f"'{a}' is not an integer")
 from random import randint
+def Game(min=1,max=10,a=None):
+    r=randint(min,max)# a random number from 1 to 10
+    while a!=r:
+        a=_intinput(f'Guess a number {min} to {max}: 'if a==None else 'Try again: ')
+        print(f'Wrong, the number is '+('higher'if a<r else'lower')+f' than {a}.'if a!=r else'')
+    print(f'Correct, the answer was {r}!')
+
 @api.on('start')
 def _():
-    print(api.c.clear+'Guess the mumber mod!')
+    print(api.c.clear+'Guess the number mod!')
 @api.register('main')
 def _():
-    def Game(min=1,max=10,a=None):
-        r=randint(min,max)# a random number from 1 to 10
-        while a!=r:
-            a=_intinput(f'Guess a number {min} to {max}: 'if a==None else 'Try again: ')
-            print(f'Wrong, the number is '+('higher'if a<r else'lower')+f' than {a}.'if a!=r else'')
-        print(f'Correct, the answer was {r}!')
     Game()
     api.exitcode(1)
